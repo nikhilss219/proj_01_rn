8c2dc464d7dea17f3b99c5c59e1a074aa245a1d3
nikhilss219 <nikhil.wilsonk@gmail.com>
2024-08-14 11:44:47 +0530

    added scrollview, textInput, image to app.tsx

--- App.tsx
+++ App.tsx
@@ -3,16 +3,40 @@ import React from 'react'
 import {
   View,
   Text,
-  SafeAreaView
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TextInput
 } from 'react-native'
 
 function App(){
-  return (<SafeAreaView>
+  return (
+  <ScrollView>
     <View>
-      <Text>Hello World!</Text>
+      <Image
+            source={{
+              uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+            }}
+            style={{width: 200, height: 200}}
+      />
     </View>
-
-  </SafeAreaView>)
+    <View
+      style={{
+          flex:1,
+          justifyContent:'center',
+          alignItems:'center'
+      }}>
+        <Text>Hello World!</Text>
+      </View>
+    <TextInput
+    style={{
+      height:40,
+      borderColor:'blue',
+      borderWidth:1,
+    }}
+    defaultValue='text inputs here'
+    />
+  </ScrollView>)
 }
 
 export default App;
